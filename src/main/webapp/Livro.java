f17ab657a8e80ce1b64964cdc7994371ce55e4f7
Andre Nunes <andrestanoga12@gmail.com>
2024-11-03 20:54:05 -0300

    First commit

---
+++ src/main/webapp/Livro.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author andre
+ */
+public class Livro {
+    private Long id;
+    private String titulo;
+    private String autor;
+    private int ano;
+    
+    
+    public Livro()
+    {
+    }
+    
+    public Livro(Long id, String titulo, String autor, int ano)
+    {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+}
+
+
+
